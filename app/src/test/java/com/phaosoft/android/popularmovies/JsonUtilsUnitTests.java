8e14a81d4d0fbb4f5728388f483df03a345213aa
jfischer <john.h.fischer@comcast.net>
2018-05-18 20:21:53 -0700

    Added some unit tests

---
+++ app/src/test/java/com/phaosoft/android/popularmovies/JsonUtilsUnitTests.java
@@ -0,0 +1,81 @@
+/*
+   Copyright 2018 John Fischer
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+package com.phaosoft.android.popularmovies;
+
+import com.phaosoft.android.popularmovies.model.Movie;
+import com.phaosoft.android.popularmovies.utils.JsonUtils;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static junit.framework.Assert.assertNotNull;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * JsonUnitls class Unit Tests
+ */
+
+public class JsonUtilsUnitTests {
+    private static final String TWO_MOVIES = "{\"page\":1,\"total_results\":19833," +
+            "\"total_pages\":992,\"results\":[{\"vote_count\":3652,\"id\":299536,\"video\":" +
+            "false,\"vote_average\":8.5,\"title\":\"Avengers: Infinity War\",\"popularity\":" +
+            "575.963952,\"poster_path\":\"\\/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg\"," +
+            "\"original_language\":\"en\",\"original_title\":\"Avengers: Infinity War\"," +
+            "\"genre_ids\":[12,878,14,28],\"backdrop_path\":" +
+            "\"\\/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg\",\"adult\":false," +
+            "\"overview\":\"more uncertain.\",\"release_date\":\"2018-04-25\"},{\"vote_count\":" +
+            "3652,\"id\":299536,\"video\":false,\"vote_average\":8.5,\"title\":" +
+            "\"Avengers: Infinity War\",\"popularity\":575.963952,\"poster_path\":" +
+            "\"\\/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg\",\"original_language\":\"en\"," +
+            "\"original_title\":\"Avengers: Infinity War\",\"genre_ids\":[12,878,14,28]," +
+            "\"backdrop_path\":\"\\/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg\",\"adult\":false," +
+            "\"overview\":\"existence itself\",\"release_date\":\"2018-04-25\"}]}";
+    private static final String A_MOVIE = "{\"vote_count\":3652,\"id\":299536,\"video\":false," +
+            "\"vote_average\":8.5,\"title\":\"Avengers: Infinity War\",\"popularity\":" +
+            "575.963952,\"poster_path\":\"\\/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg\"," +
+            "\"original_language\":\"en\",\"original_title\":\"Avengers: Infinity War\"," +
+            "\"genre_ids\":[12,878,14,28],\"backdrop_path\":" +
+            "\"\\/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg\",\"adult\":false,\"overview\":" +
+            "\"fate of Earth\",\"release_date\":\"2018-04-25\"}";
+    private static final String NULL_MOVIE = "{}";
+
+    @Test
+    public void jsonUtilsMovies() {
+        List<Movie> movies = JsonUtils.parseJsonMovie(TWO_MOVIES);
+
+        assertNotNull(movies);
+        assertEquals(movies.size(), 2);
+    }
+
+    @Test
+    public void jsonUtilsMovie() {
+        List<Movie> movie = JsonUtils.parseJsonMovie(A_MOVIE);
+
+        assertNotNull(movie);
+        assertEquals(movie.size(), 1);
+
+    }
+
+    @Test
+    public void jsonUtilsNullMovie() {
+        List<Movie> movie = JsonUtils.parseJsonMovie(NULL_MOVIE);
+
+        assertEquals(movie, null);
+    }
+}
---
+++ app/src/test/java/com/phaosoft/android/popularmovies/MovieUnitTests.java
@@ -0,0 +1,66 @@
+/*
+   Copyright 2018 John Fischer
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+*/
+
+package com.phaosoft.android.popularmovies;
+
+import com.phaosoft.android.popularmovies.model.Movie;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Movie class Unit Tests
+ */
+
+public class MovieUnitTests {
+    private String testTitle = "Movie Title";
+    private String testDate = "12-10-2014";
+    private String testPoster = "http://somepath.com/";
+    private String testSynopsis = "This is a synopsis";
+    private double testVote = 7.6;
+
+    @Test
+    public void testGetters() {
+        Movie movie = new Movie(testTitle, testDate, testPoster, testVote, testSynopsis);
+
+        assertTrue(movie.getTitle().equals(testTitle));
+        assertTrue(movie.getDate().equals(testDate));
+        assertTrue(movie.getPosterUrl().equals(testPoster));
+        assertTrue(movie.getSynopsis().equals(testSynopsis));
+        assertTrue(Math.abs(movie.getVote()) == Math.abs(testVote));
+    }
+
+    @Test
+    public void testEquals() {
+        Movie movie = new Movie(testTitle, testDate, testPoster, testVote, testSynopsis);
+
+        Movie notSame = new Movie("A", "B", "C", 5.5, "D");
+        Movie same = new Movie(testTitle, testDate, testPoster, testVote, testSynopsis);
+
+        assertTrue(movie.equals(same));
+        assertFalse(movie.equals(notSame));
+    }
+
+    @Test
+    public void testToString() {
+        Movie movie = new Movie(testTitle, testDate, testPoster, testVote, testSynopsis);
+
+        assertTrue(movie.toString().equals(testTitle + ":" + testDate + ":" + testVote));
+    }
+}
